d7b8f153ab6f507d5bce889482d9c4e5a492fc44
aimakptus <iamacactus.p@proton.me>
2025-01-25 10:01:12 -0500

    Backend terminado

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,20 +1,19 @@
+const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET;
-
 const auth = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    console.log('token recibido', token); //borrar
-    if (!token) {return res.status(401).json({message: 'Acceso denegado'})};
-
     try {
-        const decoded = jwt.verify(token, secretKey);
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+        if (!token) {
+            return res.status(401).json({ message: 'Acceso denegado.' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        res.status(401).json({message: 'Token no válido'});
+        res.status(401).json({ message: 'Token inválido o expirado.' });
     }
 };
 
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { createProduct, getProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const authMiddleware = require('../middleware/auth');
 
 // Ruta para crear un nuevo producto
-router.post('/products', createProduct);
+router.post('/products', authMiddleware, createProduct);
 
 // Ruta para obtener productos (todos o por nombre)
-router.get('/products', getProduct);
+router.get('/products', authMiddleware, getProduct);
 
 // Ruta para actualizar un producto por ID
-router.put('/products/:id', updateProduct);
+router.put('/products/:id', authMiddleware, updateProduct);
 
 // Ruta para eliminar un producto por ID
-router.delete('/products/:id', deleteProduct);
+router.delete('/products/:id', authMiddleware, deleteProduct);
 
 module.exports = router;
